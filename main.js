ddb77c086b5c3b80ecc555bf6feb045702d44c3d
shikamaru0102 <daybydaylove@gmail.com>
2022-03-18 10:26:11 +0700

    up

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const btnCategory = $$('.btn-category')
+const btnCategory = $$('.btn-category');
+const productsHeart = $$('.feature-product .feature-icon')
+const productHeartFill = $$('.feature-product .feature-icon--active')
 
 btnCategory.forEach(function(ele)
 {
     ele.style.left = `calc(50% - ${ele.clientWidth/2}px)`;
 })
+
+productsHeart.forEach(function(ele, index)
+{
+    ele.onclick = function(event)
+    {
+        event.target.classList.add('disable');
+        productHeartFill[index].classList.add('active');
+    }
+   
+})
+
+productHeartFill.forEach(function(ele, index)
+{
+    ele.onclick = function(event)
+    {
+        event.target.classList.remove('active');
+        productsHeart[index].classList.remove('disable');
+    }
+   
+})
+
+
