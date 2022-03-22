64d63a6ff1cbaae384e0023cf99465742fd40210
shikamaru0102 <daybydaylove@gmail.com>
2022-03-23 03:17:53 +0700

    thanhdz

---
+++ openFunctionNav.js
@@ -0,0 +1,26 @@
+// click appear menu-list
+
+
+const setting = $('.user-settings');
+
+setting.addEventListener('click', function() {
+    $('.header-setting').classList.toggle('active');
+});
+
+const notify = $('.notify');
+
+notify.addEventListener('click', function() {
+    $('.header__notify').classList.toggle('active');
+});
+
+const cart = $('.cart-has-product');
+
+cart.addEventListener('click', function() {
+    $('.header__cart-list').classList.toggle('active');
+});
+
+// const toolSearch = $('.tool-search');
+
+// toolSearch.addEventListener('click', function() {
+//     $('.search-form').classList.toggle('active');
+// });
