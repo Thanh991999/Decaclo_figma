66db3dd0196db5c7141cdfd5c7006ef04d07bcf8
shikamaru0102 <daybydaylove@gmail.com>
2022-03-22 22:01:00 +0700

    thanhdz

--- playVideo.js
+++ playVideo.js
@@ -2,7 +2,6 @@
 
 const videoPlayer = $('.discover-vid');
 
-
 const videoBtn = $('.discover-play');
 
 const video = $('.control-video');
@@ -11,7 +10,6 @@ const video = $('.control-video');
 
 videoPlayer.addEventListener('click', function(e)
 {
-    console.log(1)
     video.style.zIndex = '2';
     video.play();
 })
